a9983528d1825f647651efcbede5b575aef83c4d
Finn <112250336+finn200102@users.noreply.github.com>
2024-10-15 17:28:25 +0200

    Added operate method and calculator args variable

--- calculations.js
+++ calculations.js
@@ -24,7 +24,7 @@ function mul(n1, n2) {
 
 function div(n1, n2) {
   if (typeof n1 === "number" && typeof n2 === "number") {
-    if (n2 == 0) {
+    if (n2 === 0) {
       throw new Error("Divison by 0 is not allowed!");
     }
     return n1 / n2;
@@ -32,3 +32,28 @@ function div(n1, n2) {
     throw new Error("Both arguments need to be numbers");
   }
 }
+
+function operate(args) {
+  if (args.operator === operations.add) {
+    return add(args.numberOne, args.numberTwo);
+  } else if (args.operator === operations.sub) {
+    return sub(args.numberOne, args.numberTwo);
+  } else if (args.operator === operations.mul) {
+    return mul(args.numberOne, args.numberTwo);
+  } else if (args.operator === operations.div) {
+    return div(args.numberOne, args.numberTwo);
+  }
+}
+
+let operations = {
+  add: "+",
+  sub: "-",
+  mul: "*",
+  div: "/",
+};
+
+let calculatorArgs = {
+  numberOne: undefined,
+  operator: undefined,
+  numberTwo: undefined,
+};
